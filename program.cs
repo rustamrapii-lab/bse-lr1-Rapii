d5156b8fd57286f57df4877cc6e10f9a02ab74c0
Рапій Рустам <rustam.rapii@nure.ua>
2026-04-26 00:26:55 +0300

    feat: final code update

--- program.cs
+++ program.cs
@@ -1,1 +1,26 @@
-﻿Console.WriteLine("Hello, NURE!");
+﻿using System;
+
+public static class Helper
+{
+    public static int FindMax(int[] array)
+    {
+        if (array == null || array.Length == 0)
+            throw new ArgumentException("Array is empty");
+
+        int max = array[0];
+        for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] > max) max = array[i];
+        }
+        return max;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 99, 3 };
+        Console.WriteLine(Helper.FindMax(numbers));
+    }
+}
